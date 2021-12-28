b0a83b72e9a092977b3cec39a61158b1360229c9
RoGuBa <62552382+RoGuBa@users.noreply.github.com>
2021-12-28 08:21:41 +0100

    added border_flag

--- tetris.asm
+++ tetris.asm
@@ -2,28 +2,38 @@
 
 mov es, [off_screen]
 
-mov byte [x_draw], 0
-mov byte [y_draw], 0
-
-mov ax, [border_color_o]
-mov word [c_draw_o], ax
-
-mov ax, [border_color_i]
-mov [c_draw_i], ax
-
-call drawBlock
-
-mov byte [x_draw], 1
-
-call drawBlock
+call drawBorder
 
 jmp end
+
+
+drawBorder:
+
+    mov byte [x_draw], 0
+    mov byte [y_draw], 0
+
+    mov ax, [border_color_o]
+    mov word [c_draw_o], ax
+
+    mov ax, [border_color_i]
+    mov [c_draw_i], ax
+    
+    mov [border_flag], word 1
+
+    call drawBlock
+
+    ;draw border around game
+    
+    mov [border_flag], word 0
+
+    ret
 
 drawBlock:      ; x_draw
                 ; y_draw
                 ; c_draw_o
                 ; c_draw_i
-    ;draw border
+                ; border_flag
+    
     mov ax, [y_draw]    ;
     mul word [t_size]   ;calc real y pos
     add ax, [y_start]   ;add y start offset
@@ -33,50 +43,73 @@ drawBlock:      ; x_draw
     mul word [t_size]   ;calc real x pos
     add ax, [x_start]   ;add x start offset
     add ax, bx          ;sum all together for real x y pos
+        
+    mov cx, [border_flag]
+    cmp cx, 0
+    jz drawBlock_border_flag_skip
+    
+    mov bx, ax
+    mov ax, [x_screen]
+    mul word [t_size]
+    add ax, [t_size]
+    sub bx, ax
+    mov ax, bx
+    
 
+
+    ;delay  (CX:DX)      
+    mov ah, 0x86
+    mov cx, 0xF
+    mov dx, 0x0
+    int 0x15
+
+    mov ax, bx
+
+    drawBlock_border_flag_skip:
+    
     mov cx, [t_size]
     dec cx
-    drawBlock_border0: 
+    drawBlock_outer0: 
         mov dl, [c_draw_o]
         mov di, ax
         mov [es:di], dl
         
         add ax, 1
         dec cx
-        jnz drawBlock_border0        
+        jnz drawBlock_outer0        
         
         mov cx, [t_size]
         dec cx
-    drawBlock_border1:
+    drawBlock_outer1:
         mov dl, [c_draw_o]
         mov di, ax         
         mov [es:di], dl
         
         add ax, [x_screen]
         dec cx
-        jnz drawBlock_border1
+        jnz drawBlock_outer1
         
         mov cx, [t_size]
         dec cx
-    drawBlock_border2:
+    drawBlock_outer2:
         mov dl, [c_draw_o]
         mov di, ax         
         mov [es:di], dl
         
         sub ax, 1
         dec cx
-        jnz drawBlock_border2
+        jnz drawBlock_outer2
         
         mov cx, [t_size]
         dec cx
-    drawBlock_border3:
+    drawBlock_outer3:
         mov dl, [c_draw_o]
         mov di, ax         
         mov [es:di], dl
         
         sub ax, [x_screen]
         dec cx
-        jnz drawBlock_border3
+        jnz drawBlock_outer3
         
         add ax, 1
         
@@ -102,11 +135,10 @@ drawBlock:      ; x_draw
         inc ax
 
         dec bx
-        mov cx, bx
+        ;mov cx, bx
         jnz drawBlock_inner0
 
     ret
-
 
 
 end:
@@ -129,3 +161,4 @@ x_draw:         dw 0
 y_draw:         dw 0
 c_draw_o:       dw 0
 c_draw_i:       dw 0
+border_flag:    dw 0

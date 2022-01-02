fbf7face909227232ca77434d4c394c55d8dd804
RoGuBa <62552382+RoGuBa@users.noreply.github.com>
2022-01-02 14:15:32 +0100

    fixed run.sh

--- run.sh
+++ run.sh
@@ -4,39 +4,53 @@ helpFunc() {
     echo "Parameters:"
     echo -e "    none -> build and run on qemu"
     echo -e "    -h   -> open this help menu"
-    echo -e "    -i   -> build and create iso file"    
+    echo -e "    -i   -> build and create iso file"
+    echo -e "    -b   -> only build bin file"
+    echo -e "    -r   -> only run bin file"
     exit
 }
 
 build() {
+    nasm -f bin tetris.asm -o tetris.bin
     nasm -f bin stage2.asm -o stage2.bin
     nasm -f bin boot.asm -o boot.bin
 }
 
 buildIso() {
     dd if=/dev/zero of=tetris.img bs=1024 count=1440 status=none
-    dd if=boot.bin of=tetris.img seek=0 count=1 conv=notrunc status=none
+    dd if=boot.bin of=tetris.img conv=notrunc status=none
     
     mkdir iso
     cp tetris.img iso/
-    genisoimage -quiet -V 'TETRIS' -input-charset iso8859-1 -o tetris.iso \
-        -b tetris.img -hide tetris.img iso/
+    genisoimage -quiet -V 'TETRIS' -o tetris.iso \
+        -b tetris.img iso/
     rm -r iso
     
     echo "tetris.iso was generated"
-    exit 0
 }
 
 run() {
     qemu-system-x86_64 boot.bin
 }
 
-while getopts "hi" opt; do
+while getopts "ibrh" opt; do
     case $opt in
-        i ) build
+        i ) 
+            build
             buildIso
+            exit
             ;;
-        h | ? ) helpFunc
+        b ) 
+            build
+            exit
+            ;;
+        r )
+            run
+            exit
+            ;;
+        h | ? )
+            helpFunc
+            exit
             ;;
     esac
 done

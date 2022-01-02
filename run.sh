2a8121b62d3ea3e7e6aeca91d8abb2b255f41bcd
RoGuBa <62552382+RoGuBa@users.noreply.github.com>
2022-01-02 13:21:07 +0100

    added iso creation option in run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,45 @@
 #!/bin/bash
 
-#nasm -f bin boot.asm -o boot.bin
-#qemu-system-x86_64 boot.bin
+helpFunc() {
+    echo "Parameters:"
+    echo -e "    none -> build and run on qemu"
+    echo -e "    -h   -> open this help menu"
+    echo -e "    -i   -> build and create iso file"    
+    exit
+}
 
-### stage 2 ###
-nasm -f bin stage2.asm -o stage2.bin
+build() {
+    nasm -f bin stage2.asm -o stage2.bin
+    nasm -f bin boot.asm -o boot.bin
+}
 
-nasm -f bin boot.asm -o boot.bin
+buildIso() {
+    dd if=/dev/zero of=tetris.img bs=1024 count=1440 status=none
+    dd if=boot.bin of=tetris.img seek=0 count=1 conv=notrunc status=none
+    
+    mkdir iso
+    cp tetris.img iso/
+    genisoimage -quiet -V 'TETRIS' -input-charset iso8859-1 -o tetris.iso \
+        -b tetris.img -hide tetris.img iso/
+    rm -r iso
+    
+    echo "tetris.iso was generated"
+    exit 0
+}
 
-qemu-system-x86_64 boot.bin
+run() {
+    qemu-system-x86_64 boot.bin
+}
+
+while getopts "hi" opt; do
+    case $opt in
+        i ) build
+            buildIso
+            ;;
+        h | ? ) helpFunc
+            ;;
+    esac
+done
+
+build
+run
